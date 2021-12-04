71e3e27a769e4c11ba181c3b4eee9403f2909d3f
Benjamin Hodgson <bhodgson@stackoverflow.com>
2021-12-04 16:33:33 -0500

    day 1

---
+++ src/day1.rs
@@ -0,0 +1,32 @@
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<usize> {
+    input
+        .lines()
+        .map(|l| l.parse().unwrap())
+        .collect()
+}
+
+#[aoc(day1, part1)]
+pub fn part1(input: &[usize]) -> usize {
+    count_increases(input)
+}
+#[aoc(day1, part2)]
+pub fn part2(input: &[usize]) -> usize {
+    count_increases(&sum_threes(input))
+}
+
+fn count_increases(input: &[usize]) -> usize {
+    input.iter()
+        .skip(1)
+        .zip(input.iter())
+        .filter(|(cur, prev)| cur > prev)
+        .count()
+}
+
+fn sum_threes(input: &[usize]) -> Vec<usize> {
+    input.iter().skip(2)
+        .zip(input.iter().skip(1))
+        .zip(input.iter())
+        .map(|((x, y), z)| x + y + z)
+        .collect()
+}
